728d293a6296e5737e74b5167ea04206d37cee28
JohanCarette <eliabajohan@gmail.com>
2021-09-29 09:44:34 -0500

    Chapter 6 - introducing keyframe animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,19 +9,19 @@ public class PlayerController : MonoBehaviour
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
     private CharacterController characterController;
-
+	
     // Use this for initialization
-    void Start()
+	void Start ()
     {
         characterController = GetComponent<CharacterController>();
-    }
-
-    // Update is called once per frame
-    void Update()
+	}
+	
+	// Update is called once per frame
+	void Update ()
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
-    }
+	}
 
     void FixedUpdate()
     {
